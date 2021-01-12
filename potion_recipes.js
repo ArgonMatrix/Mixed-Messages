e3b39bc65de98353ccb04b28f3aa8771d6c7d49b
Zach Henderson <argonmatrix@hotmail.com>
2021-01-12 08:27:02 -0700

    Added initial data sets to pull from.

---
+++ potion_recipes.js
@@ -0,0 +1,41 @@
+// Elements of Potion Name
+const potionSynonyms = ["Potion of",
+                        "Essence of",
+                        "Elixir of", 
+                        "Distilled", 
+                        "Liquid"];
+const potionTypes = ["Invisibility", 
+                     "Super Strength", 
+                     "Love", 
+                     "Dexterity", 
+                     "Luck"];
+
+
+// Selections of Ingredients & Preparation Methods
+const ingredients = {
+    liquid: ["Unicorn's Milk",
+             "Shadow Syrup",
+             "Cosmic Water",
+             "Ambrosia",
+             "Nymph's Blood"],
+    primary: ["Vampire's Tooth",
+              "Basilisk's Eye",
+              "Root of 1,000-Year-Old Tree",
+              "Brimstone",
+              "Spirits' Bane"],
+    secondary: ["Cockatrice Egg",
+                "Lunar Seaweed",
+                "Leprechaun's Boot",
+                "Golden Apricot",
+                "Ice Diamond"],
+    spice: ["Powdered Pig's Feet",
+            "Salamander Salt",
+            "Flaked Toadskin",
+            "Pulverized Dragon Scales",
+            "Starlight"],
+    garnish: ["Black Toadstool",
+              "Ogre's Finger Bone",
+              "Tuft of Garnakk Fur",
+              "Sanctified Leaf",
+              "Nightmare Globule"]
+}
