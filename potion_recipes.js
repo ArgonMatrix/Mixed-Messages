b20d723b49a81af845ad2b68d75e6c0523c22205
Zach Henderson <argonmatrix@hotmail.com>
2021-01-12 22:59:44 -0700

    Updated format & started work on preparation methods.

--- potion_recipes.js
+++ potion_recipes.js
@@ -1,41 +1,18 @@
 // Elements of Potion Name
-const potionSynonyms = ["Potion of",
-                        "Essence of",
-                        "Elixir of", 
-                        "Distilled", 
-                        "Liquid"];
-const potionTypes = ["Invisibility", 
-                     "Super Strength", 
-                     "Love", 
-                     "Dexterity", 
-                     "Luck"];
+const potionSynonyms = ["Potion of", "Essence of", "Elixir of", "Distilled", "Liquid"];
+const potionTypes = ["Invisibility", "Super Strength", "Love", "Dexterity", "Luck"];
 
 
 // Selections of Ingredients & Preparation Methods
 const ingredients = {
-    liquid: ["Unicorn's Milk",
-             "Shadow Syrup",
-             "Cosmic Water",
-             "Ambrosia",
-             "Nymph's Blood"],
-    primary: ["Vampire's Tooth",
-              "Basilisk's Eye",
-              "Root of 1,000-Year-Old Tree",
-              "Brimstone",
-              "Spirits' Bane"],
-    secondary: ["Cockatrice Egg",
-                "Lunar Seaweed",
-                "Leprechaun's Boot",
-                "Golden Apricot",
-                "Ice Diamond"],
-    spice: ["Powdered Pig's Feet",
-            "Salamander Salt",
-            "Flaked Toadskin",
-            "Pulverized Dragon Scales",
-            "Starlight"],
-    garnish: ["Black Toadstool",
-              "Ogre's Finger Bone",
-              "Tuft of Garnakk Fur",
-              "Sanctified Leaf",
-              "Nightmare Globule"]
+    liquid: ["Unicorn's Milk", "Shadow Syrup", "Cosmic Water", "Ambrosia", "Nymph's Blood"],
+    primary: ["Vampire's Tooth", "Basilisk's Eye", "Root of 1,000-Year-Old Tree", "Brimstone", "Spirits' Bane"],
+    secondary: ["Cockatrice Egg", "Lunar Seaweed", "Leprechaun's Toe", "Golden Apricot", "Ice Diamond"],
+    spice: ["Powdered Pig's Feet", "Salamander Salt", "Flaked Toadskin", "Pulverized Dragon Scales", "Red Starlight"],
+    garnish: ["Black Toadstool", "Ogre's Finger Bone", "Tuft of Garnakk Fur", "Sanctified Leaf", "Nightmare Globule"],
+
+    // Preparation Methods
+    prepLiquid(liquid) {
+        
+    }
 }
